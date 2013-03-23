ab04f41152fae934778c4e017987cba6fcc37bba
Paul Zaich <pzaich@gmail.com>
2013-03-23 17:53:15 +0900

    add basic layout for search and icons on header

---
+++ app/assets/javascripts/static.js.coffee
@@ -0,0 +1,2 @@
+$ ->
+  $('.nav-utils').tooltip()
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def home
-    @museums = Museum.all.to_gmaps4rails
+    #@museums = Museum.all.to_gmaps4rails
     @paintings = Painting.to_mappable_json
   end
 end

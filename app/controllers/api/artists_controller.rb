0b0ff29e2e5de7bc1b7739adf6cd6574bd3dc0d6
Paul Zaich <pzaich@gmail.com>
2014-03-29 17:06:45 -0700

    get basic respond_to :json inheritance working

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,5 +1,6 @@
 module Api
   class ArtistsController < ApiController
+    
     def index
       @artists = Artist.search_by_name(params[:query])
       render :json => @artists.pluck(:name)
--- app/controllers/api/museums_controller.rb
+++ app/controllers/api/museums_controller.rb
@@ -1,5 +1,5 @@
 module Api
-  class MuseumsController < ApplictionController
+  class MuseumsController < ApiController
     def index
       @museums = Search.new(params[:query], params[:location]).museums
       flash[:notice] = "Sorry we couldn't find anything nearby that matches your search."
@@ -10,13 +10,7 @@ module Api
       @museum = Museum.find params[:id]
       @artist = Artist.search_by_name(params[:query]).first
       @paintings = @museum.paintings(params[:query])
-      respond_to do |f|
-        f.js
-        f.html do
-          params[:location] = @museum.to_coordinates
-          render 'static/home'
-        end
-      end
+      respond_with @museum
     end
   end
 end

22167d00c92162dea7ef2016b4a02f37a48149fc
Paul Zaich <pzaich@gmail.com>
2015-02-01 22:32:22 -0800

    another fix

--- config/initializers/haml_templates.rb
+++ config/initializers/haml_templates.rb
@@ -1,17 +1,17 @@
-# require 'haml'
+require 'haml'
 
 Rails.application.assets.register_mime_type 'text/html', '.html'
 Rails.application.assets.register_engine '.haml', Tilt::HamlTemplate
 
-# class HamlTemplate < Tilt::HamlTemplate
-#   def prepare
-#     @options = @options.merge :format => :html5
-#     super
-#   end
-# end
+class HamlTemplate < Tilt::HamlTemplate
+  def prepare
+    @options = @options.merge :format => :html5
+    super
+  end
+end
 
-# config.before_initialize do |app|
-#   require 'sprockets'
-#   Sprockets::Engines #force autoloading
-#   Sprockets.register_engine '.haml', HamlTemplate
-# end
+config.before_initialize do |app|
+  require 'sprockets'
+  Sprockets::Engines #force autoloading
+  Sprockets.register_engine '.haml', HamlTemplate
+end

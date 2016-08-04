cee2035203088c6f260b3d5b35fb6f7b241196ea
Paul Zaich <pzaich@gmail.com>
2016-08-04 10:49:50 -0700

    remove geocoder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-
-  def current_location
-    @current_location ||= request.location.try(:data)
-  end
-  helper_method :current_location
-
 end
